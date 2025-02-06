fb71780a276812cc676fe7634c52143260daec50
typhone-99 <typhoooone.99@gmail.com>
2025-02-06 20:05:33 +0900

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,41 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
-import { FiLink } from "react-icons/fi";
 
 export default function Home() {
   return (
     <div>
       <div className="my-8 md:w-1/2 mx-4 md:mx-auto">
-        <div className="p-4 rounded-lg border border-slate-200 bg-white">
+        <div className="p-4 rounded-lg border border-zinc-200 bg-white">
           <div className="flex">
             <Image src="/icon.jpg" alt="Icon" width={100} height={100} className="aspect-square w-16 h-16 rounded-full"  />
             <div className="ml-4">
-              <h1 className="text-xl font-semibold">Typhone</h1>
-              <p className="mt-2 text-slate-400 text-sm">I&apos;m TyPhone, a middle schooler living in Japan. I mainly focus on front-end development.</p>
-              <div className="flex items-center mt-2 gap-2">
-                <Link href="/"><FiLink className="text-xl text-slate-400" /></Link>
-                <Link href="https://github.com/tyseries" rel="noreferrer noopener" target="_blank"><FaGithub className="text-xl text-slate-400" /></Link>
-                <Link href="https://zenn.dev/tyseries" rel="noreferrer noopener" target="_blank"><Image src="/zenn.svg" alt="Zenn" width={100} height={100} className="w-5 h-5 aspect-square" /></Link>
+              <h1 className="text-xl font-bold">Typhone</h1>
+              <p className="my-2">I&apos;m TyPhone, a middle schooler living in Japan. I mainly focus on front-end development.</p>
+              <div className="flex items-center gap-2 text-zinc-400 flex-wrap">
+                <Link href="https://github.com/tyseries" rel="noreferrer noopener" target="_blank">
+                  <div className="px-2 py-0.5 border rounded-lg border-zinc-200 flex items-center shadow-sm">
+                    <FaGithub className="mr-2" /><p>@tyseries</p>
+                  </div>
+                </Link>
+                <Link href="https://zenn.dev/tyseries" rel="noreferrer noopener" target="_blank">
+                  <div className="px-2 py-0.5 border rounded-lg border-zinc-200 flex items-center shadow-sm">
+                    <Image src="/zenn.svg" alt="Zenn Logo" width={100} height={100} className="w-4 h-4 aspect-square mr-2" /><p>@tyseries</p>
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
-          <div className="mt-4 flex items-center gap-2 flex flex-wrap text-sm">
-            <p className="px-2 py-0.5 rounded bg-slate-200 text-slate-600 w-fit">HTML</p>
-            <p className="px-2 py-0.5 rounded bg-slate-200 text-slate-600 w-fit">CSS</p>
-            <p className="px-2 py-0.5 rounded bg-slate-200 text-slate-600 w-fit">JavaScript</p>
-            <p className="px-2 py-0.5 rounded bg-slate-200 text-slate-600 w-fit">TypeScript</p>
-            <p className="px-2 py-0.5 rounded bg-slate-200 text-slate-600 w-fit">React</p>
-            <p className="px-2 py-0.5 rounded bg-slate-200 text-slate-600 w-fit">Next.js</p>
+          <div className="mt-4 flex items-center gap-2 flex-wrap text-zinc-600">
+            <p className="px-2 py-0.5 rounded bg-zinc-200 w-fit">HTML</p>
+            <p className="px-2 py-0.5 rounded bg-zinc-200 w-fit">CSS</p>
+            <p className="px-2 py-0.5 rounded bg-zinc-200 w-fit">JavaScript</p>
+            <p className="px-2 py-0.5 rounded bg-zinc-200 w-fit">TypeScript</p>
+            <p className="px-2 py-0.5 rounded bg-zinc-200 w-fit">React</p>
+            <p className="px-2 py-0.5 rounded bg-zinc-200 w-fit">Next.js</p>
           </div>
         </div>
-        <div className="mt-8 p-4 rounded-lg border border-slate-200 bg-white">
+        <div className="mt-8 p-4 rounded-lg border border-zinc-200 bg-white">
           <h2 className="text-xl font-semibold">Projects</h2>
           <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             <Link href="https://waterui.vercel.app" target="_blank" rel="noopener noreferrer" className="border border-slate-200 rounded-lg p-2 shadow-sm">
@@ -38,7 +44,7 @@ export default function Home() {
             </Link>
           </div>
         </div>
-        <div className="mt-8 p-4 rounded-lg border border-slate-200 bg-white">
+        <div className="mt-8 p-4 rounded-lg border border-zinc-200 bg-white">
           <h2 className="text-xl font-semibold">Timeline</h2>
           <div className="space-y-4 mt-4 ml-2">
             <div className="flex items-center"><div className="w-4 h-4 rounded-full border-4 aspect-square border-slate-400 mr-2" /><p className="text-sm">I&apos;ve launched this website. 2025/02/01</p></div>
